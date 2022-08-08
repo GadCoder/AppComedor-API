fdd5a8636798417db57889d3765806b82ab12e36
PhoenixBlue02 <phoenixblue02@pop-os.localdomain>
2022-08-08 17:04:46 -0500

    ticket 4

--- schemas/tickets.py
+++ schemas/tickets.py
@@ -4,21 +4,21 @@ from pydantic import BaseModel
 class TicketBase(BaseModel):
     ticket_id: Optional[int] = None
     code: Optional[str] = None
-    shift: Optional[int] = None
+    shift: Optional[str] = None
     hour: Optional[str] = None
     is_scanned: Optional[bool] = None
 
 class TicketCreate(TicketBase):
     ticket_id: int
     code: str
-    shift: int
+    shift: str
     hour: str
     is_scanned: bool
 
 class ShowTicket(TicketBase):
     ticket_id: int
     code: str
-    shift: int
+    shift: str
     hour: str
     is_scanned: bool
 
