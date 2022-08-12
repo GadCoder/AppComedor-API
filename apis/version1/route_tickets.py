45d61a6f8dfeb338d57c7a14c184e0778ab15bf8
PhoenixBlue02 <phoenixblue02@pop-os.localdomain>
2022-08-12 16:57:49 -0500

    Ticket 7 2.0

--- apis/version1/route_tickets.py
+++ apis/version1/route_tickets.py
@@ -2,14 +2,14 @@ from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from fastapi import Depends
 
-from schemas.tickets import TicketCreate, ShowTicket
+from schemas.tickets import TicketCreate
 from db.session import get_db
 from db.repository.tickets import create_new_ticket
 
 router = APIRouter()
 
 
-@router.post("/create-ticket/", response_model=ShowTicket)
+@router.post("/create-ticket/")
 def create_ticket(ticket: TicketCreate, db: Session = Depends(get_db)):
     ticket = create_new_ticket(ticket=ticket, db=db)
     return ticket
--- db/repository/tickets.py
+++ db/repository/tickets.py
@@ -4,7 +4,7 @@ from schemas.tickets import TicketCreate
 from db.models.tickets import Ticket
 
 def create_new_ticket(ticket:TicketCreate, db:Session):
-    ticket=Ticket(ticket_id=ticket.ticket_id,
+    ticket = Ticket(ticket_id=ticket.ticket_id,
                   code=ticket.code,
                   shift=ticket.shift,
                   hour=ticket.hour,
