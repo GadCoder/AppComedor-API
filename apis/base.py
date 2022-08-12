f841aa14baf7c6ed8aca209f81c1859a388ef972
PhoenixBlue02 <phoenixblue02@pop-os.localdomain>
2022-08-12 12:24:41 -0500

    no funciona tickets :c

--- apis/base.py
+++ apis/base.py
@@ -5,7 +5,6 @@ from apis.version1 import route_users
 from apis.version1 import route_dishes
 from apis.version1 import route_login
 from apis.version1 import route_bugs
-from apis.version1 import route_tickets
 
 api_router = APIRouter()
 api_router.include_router(route_general_pages.general_pages_router, prefix="", tags=["general_pages"])
@@ -13,4 +12,3 @@ api_router.include_router(route_users.router, prefix="/users", tags=["users"])
 api_router.include_router(route_dishes.router, prefix="/dishes", tags=["dishes"])
 api_router.include_router(route_login.router, prefix="/login", tags=["login"])
 api_router.include_router(route_bugs.router, prefix="/bugs", tags=["bugs"])
-api_router.include_router(route_tickets.router, prefix="/tickets", tags=["tickets"])
--- db/base.py
+++ db/base.py
@@ -1,5 +1,4 @@
 from db.base_class import Base
 from db.models.users import User
 from db.models.dishes import Dish
-from db.models.tickets import Ticket
 
