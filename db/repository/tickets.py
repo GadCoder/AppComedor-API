3061e4c094eef1b719f03735ae52a5332cf069ed
German <german@Germans-Mac-Pro.local>
2022-08-12 20:50:30 -0500

    ticked

--- db/repository/tickets.py
+++ db/repository/tickets.py
@@ -4,11 +4,8 @@ from schemas.tickets import TicketCreate
 from db.models.tickets import Ticket
 
 def create_new_ticket(ticket:TicketCreate, db:Session):
-    ticket = Ticket(ticket_id=ticket.ticket_id,
-                  code=ticket.code,
-                  shift=ticket.shift,
-                  hour=ticket.hour,
-                  is_scanned=False)
+
+    ticket = Ticket(**ticket.dict())
     db.add(ticket)
     db.commit()
     db.refresh(ticket)
